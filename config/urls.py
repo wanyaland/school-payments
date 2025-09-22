0e18e2a0c6fd1ee2fa3d12c21683e21dfbf513e7
Harold Wanyama <wanyaland@gmail.com>
2025-09-23 03:31:52 +0400

    Added health endpoints

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/webhooks/", include("webhooks.urls")),
     path("api/qbo/", include("qbo.urls")),
+    path('health/', health_check),
 ]
